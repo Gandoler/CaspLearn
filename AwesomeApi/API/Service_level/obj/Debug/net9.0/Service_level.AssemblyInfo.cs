eb4d73ec9c701261bf947a38c40e660bb2c82b6c
glkru <gl.krutoi@mail.ru>
2025-09-14 17:13:57 +0300

    FEAT(Tests) : добавил файл тестов ч2

--- AwesomeApi/API/Service_level/obj/Debug/net9.0/Service_level.AssemblyInfo.cs
+++ AwesomeApi/API/Service_level/obj/Debug/net9.0/Service_level.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service_level")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d014e6f48d7f198166320d3929c850c87c9f6ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f7da954c04287e01c261dfa374b26580d3003c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service_level")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service_level")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
