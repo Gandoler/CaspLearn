2434eefb52c3568795db17ddc0e165fb8b570f0e
glkru <gl.krutoi@mail.ru>
2025-09-14 10:49:15 +0300

    FEAT (API) : пересел за другой комп сменилиись все пути

--- AwesomeApi/ALltrash/obj/Debug/net9.0/ALltrash.AssemblyInfo.cs
+++ AwesomeApi/ALltrash/obj/Debug/net9.0/ALltrash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ALltrash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810fe7e8e944af3129a1eeecff2460c2acae988a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152c619a5d0c0d552d136067a481ec24b6892e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("ALltrash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ALltrash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AwesomeApi/API/AwesomApi/obj/Debug/net9.0/AwesomApi.AssemblyInfo.cs
+++ AwesomeApi/API/AwesomApi/obj/Debug/net9.0/AwesomApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AwesomApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df203d081d5b65091f9e493b7edf668c93fe3e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152c619a5d0c0d552d136067a481ec24b6892e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("AwesomApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AwesomApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
