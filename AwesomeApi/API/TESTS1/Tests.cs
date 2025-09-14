d6f7da954c04287e01c261dfa374b26580d3003c
glkru <gl.krutoi@mail.ru>
2025-09-14 15:58:47 +0300

    -мусор

---
+++ AwesomeApi/API/TESTS1/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TESTS1
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
